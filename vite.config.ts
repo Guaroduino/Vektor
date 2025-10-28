b6fd96d207567237ebaeab6c01e6a005427562d6
Guaroduino <Guaroduino@gmail.com>
2025-10-27 22:13:35 -0400

    publish

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Set the correct base path for GitHub Pages project sites:
+  // https://<username>.github.io/<repo>/ -> base should be '/<repo>/'
+  base: '/Vektor/',
+});
